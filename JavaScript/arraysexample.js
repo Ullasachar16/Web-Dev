4edc407397a3a45fc8d219d45cace23f9bd1f7b4
Ullasachar16 <ullasachar16@gmail.com>
2023-02-04 11:35:18 +0530

    initial commit

---
+++ JavaScript/arraysexample.js
@@ -0,0 +1,9 @@
+var guestList=["akshay","bhaskar","chaithanya","dhanush",
+"kruthika","sujay","suraj","ullas"];
+var name=prompt("Enter your name!!!");
+if(guestList.includes(name)){
+    alert("Welcome!!!");
+}else{
+    alert("Sorry ,maybe next time");
+}
+
---
+++ JavaScript/basicarithmetic.js
@@ -0,0 +1,3 @@
+var dogAge=prompt("Enter your dog age:");
+var humanAge=((dogAge-2)*4)+21;
+alert("Your Dog is "+humanAge+" years old in human years.");
