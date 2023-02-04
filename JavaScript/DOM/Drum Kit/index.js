7885a85d0c3dd8eff91d2fcdc13628d29e8a9d3c
Ullasachar16 <ullasachar16@gmail.com>
2023-02-04 12:34:34 +0530

    minor change

--- JavaScript/DOM/Drum Kit/index.js
+++ JavaScript/DOM/Drum Kit/index.js
@@ -1,6 +1,6 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i=0; i<numberOfDrumButtons; i++){
-    document.querySelectorAll("drum")[i].addEventListener("click", function (){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
     alert("I got clicked!");
     });
 }
