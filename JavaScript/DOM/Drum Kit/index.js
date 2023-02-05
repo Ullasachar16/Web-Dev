43beb7fbac008d1048ef401cd01ec3170aaf4417
Ullasachar16 <ullasachar16@gmail.com>
2023-02-05 13:26:47 +0530

    changes

--- JavaScript/DOM/Drum Kit/index.js
+++ JavaScript/DOM/Drum Kit/index.js
@@ -5,33 +5,33 @@ for(var i=0; i<numberOfDrumButtons; i++){
         switch (buttonInnerHTML) {
             case "w":
                 var audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
-            break;
+                tom1.play();
+                break;
             
             case "a":
                 var audio = new Audio("sounds/tom-2.mp3");
                 audio.play();
-            break;
+                break;
             case "s":
                 var audio = new Audio("sounds/tom-3.mp3");
                 audio.play();
-            break;
+                break;
             case "d":
                 var audio = new Audio("sounds/tom-4.mp3");
                 audio.play();
-            break;
+                break;
             case "j":
                 var audio = new Audio("sounds/snare.mp3");
                 audio.play();
-            break;
+                break;
             case "k":
                 var audio = new Audio("sounds/crash.mp3");
                 audio.play();
-            break;
+                break;
             case "l":
                 var audio = new Audio("sounds/kick.mp3");
                 audio.play();
-            break;
+                break;
             default:
                     break;
         }
