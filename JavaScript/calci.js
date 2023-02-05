a58410fa3885c3af2f7de940da8ce8c313b1ec10
Ullasachar16 <ullasachar16@gmail.com>
2023-02-05 12:51:39 +0530

    added calci code

--- JavaScript/calci.js
+++ JavaScript/calci.js
@@ -14,10 +14,10 @@ function mod(num1,num2){
     return num1%num2;
 }
 function calci(num1,num2,operator){
-    operator(num1,num2);
+    return operator(num1,num2);
 }
-calci(2,3,add);
-calci(4,3,sub);
-calci(5,3,mul);
-calci(9,3,div);
-calci(10,3,mod);
+console.log(calci(2,3,add));
+console.log(calci(4,3,sub));
+console.log(calci(5,3,mul));
+console.log(calci(9,3,div));
+console.log(calci(10,3,mod));
