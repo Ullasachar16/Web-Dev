e1d70c9ce0fc65f2a92c7f8acb2b9920885a7b8a
Ullasachar16 <ullasachar16@gmail.com>
2023-02-05 13:09:34 +0530

    changing color of button throught event listeners

--- JavaScript/DOM/Drum Kit/index.js
+++ JavaScript/DOM/Drum Kit/index.js
@@ -1,7 +1,7 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i=0; i<numberOfDrumButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        console.log(this.style.color="white");
+        this.style.color="white";
     });
 }
 
