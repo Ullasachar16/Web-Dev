477f27d2f8cd0dd280542bd44f80ab1b1671c6ae
Ullasachar16 <ullasachar16@gmail.com>
2023-02-06 12:31:11 +0530

    website animations with jquery

--- JavaScript/jQuery/index.js
+++ JavaScript/jQuery/index.js
@@ -1,16 +1,6 @@
-$("h1").before("<button>New</button");
 
-$("h1").after("<button>New</button");
-
-$("h1").prepend("<button>New</button");
-
-$("h1").append("<button>New</button");
-
-
-
-$(document).keypress(function(event){
-    $("h1").text(event.key);
+$("button").on("click",function(){
+    $("h1").hide();
 });
 
-$("button").remove();
 
