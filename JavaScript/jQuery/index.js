932f1590b0c7c85cb1217c6cb28fc32ec15522cc
Ullasachar16 <ullasachar16@gmail.com>
2023-02-06 12:28:17 +0530

    adding and removing elements from html

--- JavaScript/jQuery/index.js
+++ JavaScript/jQuery/index.js
@@ -1,3 +1,11 @@
+$("h1").before("<button>New</button");
+
+$("h1").after("<button>New</button");
+
+$("h1").prepend("<button>New</button");
+
+$("h1").append("<button>New</button");
+
 $(document).keypress(function(event){
     $("h1").text(event.key);
 });
