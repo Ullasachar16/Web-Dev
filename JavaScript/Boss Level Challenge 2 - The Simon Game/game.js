023e9cb44f4702b30c52f5da564183c0d046768b
Ullasachar16 <ullasachar16@gmail.com>
2023-02-07 12:26:52 +0530

    added another empty array

--- JavaScript/Boss Level Challenge 2 - The Simon Game/game.js
+++ JavaScript/Boss Level Challenge 2 - The Simon Game/game.js
@@ -1,5 +1,7 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
 
 function newSequence(){
     var randomNumber = Math.floor(Math.random()*3)+1;
+    var randomChosenColor = Math.random(buttonColors);
 }
