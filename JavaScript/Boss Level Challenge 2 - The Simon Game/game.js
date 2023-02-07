df8e93f56be2ee350617e6fedabd2ccafcc4537d
Ullasachar16 <ullasachar16@gmail.com>
2023-02-07 12:34:04 +0530

    slight changes

--- JavaScript/Boss Level Challenge 2 - The Simon Game/game.js
+++ JavaScript/Boss Level Challenge 2 - The Simon Game/game.js
@@ -2,9 +2,9 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function newSequence(){
-    var randomNumber = Math.floor(Math.random()*3)+1;
-    var randomChosenColor = Math.random(buttonColors);
-    var gamePattern = gamePattern.push(randomChosenColor);
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColor);
 }
 
 newSequence();
