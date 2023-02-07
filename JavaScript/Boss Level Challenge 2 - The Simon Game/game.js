fc9f628489ba834bbd1960c06ea20e7e0ff8a1b5
Ullasachar16 <ullasachar16@gmail.com>
2023-02-07 12:58:29 +0530

    added on click function

--- JavaScript/Boss Level Challenge 2 - The Simon Game/game.js
+++ JavaScript/Boss Level Challenge 2 - The Simon Game/game.js
@@ -4,6 +4,13 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+$("btn").on("click",function(){
+    
+    var userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+});
+
 function newSequence(){
 
     var randomNumber = Math.floor(Math.random()*4);
@@ -17,6 +24,3 @@ $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
 audio.play();
 
-$("button").on("click",function(){
-    var userChosenColor = 
-});
