757b8f8041aa300d0f42ec03875cf25253c88ec3
Ullasachar16 <ullasachar16@gmail.com>
2023-02-13 12:16:53 +0530

    added another functions

--- JavaScript/Intro-to-node/index.js
+++ JavaScript/Intro-to-node/index.js
@@ -3,5 +3,7 @@ var supervillains = require("SuperVillains");
 var superheroes = require("Superheroes");
 var mySuperVillainName = supervillains.random();
 var mySuperHeroName = superheroes.random();
+var allSuperHeroesNames = superheroes.all();
 console.log(mySuperHeroName);
 console.log(mySuperVillainName);
+console.log(allSuperHeroesNames);
