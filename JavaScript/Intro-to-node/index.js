e9a5bcdf259b8c991a8ca84b3a1b7c74e84fb1ef
Ullasachar16 <ullasachar16@gmail.com>
2023-02-13 12:18:49 +0530

    added another

--- JavaScript/Intro-to-node/index.js
+++ JavaScript/Intro-to-node/index.js
@@ -4,6 +4,8 @@ var superheroes = require("Superheroes");
 var mySuperVillainName = supervillains.random();
 var mySuperHeroName = superheroes.random();
 var allSuperHeroesNames = superheroes.all;
+var allSuperVillainsName = supervillains.all;
 console.log(mySuperHeroName);
 console.log(mySuperVillainName);
 console.log(allSuperHeroesNames);
+console.log(allSuperVillainsName);
