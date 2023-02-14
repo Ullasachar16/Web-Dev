b3a56f0bac301dbff8a439acca56e521a079235c
Ullasachar16 <ullasachar16@gmail.com>
2023-02-14 11:45:07 +0530

    slight change

--- JavaScript/my-express-server/server.js
+++ JavaScript/my-express-server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",function(request,response){
-    console.log("request");
+    console.log(request);
 })
 
 app.listen(3000,function(){
