e8e3dcfcee17761f6154ebe71113f97b4c2e020b
Ullasachar16 <ullasachar16@gmail.com>
2023-02-14 11:52:29 +0530

    added another route

--- JavaScript/my-express-server/server.js
+++ JavaScript/my-express-server/server.js
@@ -10,6 +10,10 @@ app.get("/contact",function(req,res){
     res.send("contact me at: ullasachar16@gmail.com");
 });
 
+app.get("/about",function(req,res){
+    res.send("im just a new beginner");
+});
+
 app.listen(3000,function(){
     console.log("server started on port 3000");
 });
