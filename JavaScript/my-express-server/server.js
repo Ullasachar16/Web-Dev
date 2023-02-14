1ea95cdf7da7c9770dc8b9d80ed681a275ac3add
Ullasachar16 <ullasachar16@gmail.com>
2023-02-14 11:46:32 +0530

    changes

--- JavaScript/my-express-server/server.js
+++ JavaScript/my-express-server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",function(request,response){
-    console.log(request);
+    response.send("hello");
 })
 
 app.listen(3000,function(){
