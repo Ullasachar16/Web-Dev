91e584e4cf68f948ed01f8f1f4cce72eb4a4cb3c
Ullasachar16 <ullasachar16@gmail.com>
2023-02-14 12:47:16 +0530

    done with bmi

--- JavaScript/my-express-server/Calculator/calculator.js
+++ JavaScript/my-express-server/Calculator/calculator.js
@@ -10,7 +10,21 @@ app.get("/",function(req,res){
 });
 
 app.post("/",function(req,res){
-    res.send("Thanks for posting");
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("The Result of the calculation is "+result);
+});
+
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+}); 
+
+app.post("/bmicalculator",function(req,res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight / (height*height);
+    res.send("Your BMI is "+bmi);
 });
 
 app.listen(3000,function(){
