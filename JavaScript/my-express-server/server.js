541698f52060391b51bb62a60266de67c097de30
Ullasachar16 <ullasachar16@gmail.com>
2023-02-14 11:49:28 +0530

    slight changes

--- JavaScript/my-express-server/server.js
+++ JavaScript/my-express-server/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",function(request,response){
+app.get("/",function(req,res){
     response.send("<h1>Hello World!</h1>");
-})
+});
 
 app.listen(3000,function(){
     console.log("server started on port 3000");
