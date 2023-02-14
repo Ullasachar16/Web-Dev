abb188fac627aed40885709f02bb1d7a16878356
Ullasachar16 <ullasachar16@gmail.com>
2023-02-14 11:51:45 +0530

    added another route

--- JavaScript/my-express-server/server.js
+++ JavaScript/my-express-server/server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/",function(req,res){
-    response.send("<h1>Hello World!</h1>");
+    res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/contact",function(req,res){
+    res.send("contact me at: ullasachar16@gmail.com");
 });
 
 app.listen(3000,function(){
