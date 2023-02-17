d1ee460a3ca8ff45ff11ca91c9fa0ea7c2ca8e1c
Ullasachar16 <ullasachar16@gmail.com>
2023-02-17 13:21:00 +0530

    getting required data

--- JavaScript/API/WeatherProject/app.js
+++ JavaScript/API/WeatherProject/app.js
@@ -9,11 +9,8 @@ app.get("/",function(req,res){
         response.on("data",function(data){
             const weatherData = JSON.parse(data);
             console.log(weatherData);
-            const object = {
-                name: "Ullas",
-                favoriteFood: "Biryani"
-            }
-            console.log(JSON.stringify(object));
+            const temp = weatherData.main.temp;
+            console.log(temp);
         });
     });
     res.send("Server is up and running");
