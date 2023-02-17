255ec8a497d6709fd240ccdb2343f3e445c2240c
Ullasachar16 <ullasachar16@gmail.com>
2023-02-17 13:57:44 +0530

    connected

--- JavaScript/API/WeatherProject/app.js
+++ JavaScript/API/WeatherProject/app.js
@@ -11,28 +11,27 @@ app.get("/",function(req,res){
         });
 
         app.post("/",function(req,res){
-           console.log(req.body.cityName) ;
-            
+             const query = req.body.cityName;
+        const apiKey = "9c745cac0b43efe3f115d94f1e518258";
+        const unit = "metric";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit+"";
+        https.get(url,function(response){
+            console.log(response.statusCode);
+            response.on("data",function(data){
+                const weatherData = JSON.parse(data);
+                console.log(weatherData);
+                const temp = weatherData.main.temp;
+                const weatherDescription = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const imageURL = "https://openweathermap.org/img/wn/" +icon+ "@2x.png";
+                res.write("<p>The Weather is currently "+weatherDescription+"</p>");
+                res.write("<h1>The Temperature in London is "+temp+" degree celcius.</h1>");
+                res.write("<img src=" +imageURL+ ">");
+                res.send();
+                });
+            });
         });
-        // const query = "London";
-        // const apiKey = "9c745cac0b43efe3f115d94f1e518258";
-        // const unit = "metric";
-        // const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit+"";
-        // https.get(url,function(response){
-        //     console.log(response.statusCode);
-        //     response.on("data",function(data){
-        //         const weatherData = JSON.parse(data);
-        //         console.log(weatherData);
-        //         const temp = weatherData.main.temp;
-        //         const weatherDescription = weatherData.weather[0].description;
-        //         const icon = weatherData.weather[0].icon;
-        //         const imageURL = "https://openweathermap.org/img/wn/" +icon+ "@2x.png";
-        //         res.write("<p>The Weather is currently "+weatherDescription+"</p>");
-        //         res.write("<h1>The Temperature in London is "+temp+" degree celcius.</h1>");
-        //         res.write("<img src=" +imageURL+ ">");
-        //         res.send();
-//     });
-// });
+       
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000.");
