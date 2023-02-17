85ee47d52588cc556858b73a388702067e3fa9a8
Ullasachar16 <ullasachar16@gmail.com>
2023-02-17 13:16:41 +0530

    added json

--- JavaScript/API/WeatherProject/app.js
+++ JavaScript/API/WeatherProject/app.js
@@ -7,7 +7,8 @@ app.get("/",function(req,res){
     https.get(url,function(response){
         console.log(response.statusCode);
         response.on("data",function(data){
-            console.log(data);
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
         });
     });
     res.send("Server is up and running");
