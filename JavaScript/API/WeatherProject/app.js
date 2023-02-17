1b38282be9f5d9622acf1af3f410880a45a5fa64
Ullasachar16 <ullasachar16@gmail.com>
2023-02-17 13:09:39 +0530

    getting status code

--- JavaScript/API/WeatherProject/app.js
+++ JavaScript/API/WeatherProject/app.js
@@ -5,15 +5,10 @@ const https = require("https");
 app.get("/",function(req,res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=9c745cac0b43efe3f115d94f1e518258&units=metric";
     https.get(url,function(response){
-        console.log(response);
+        console.log(response.statusCode);
     });
     res.send("Server is up and running");
 });
-
-
-
-
-
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000.");
