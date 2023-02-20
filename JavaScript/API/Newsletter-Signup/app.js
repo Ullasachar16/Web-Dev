45a36336aa51fc367b450e59bbf090c3d7b0b205
Ullasachar16 <ullasachar16@gmail.com>
2023-02-20 13:02:08 +0530

    adding more code

--- JavaScript/API/Newsletter-Signup/app.js
+++ JavaScript/API/Newsletter-Signup/app.js
@@ -3,8 +3,13 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
+
+app.post("/",function(req,res){
+    var firstName = req.body
+});
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html");
