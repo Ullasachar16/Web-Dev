711f101bd0209d22d0e5f8a1195b1a2c69a85772
Ullasachar16 <ullasachar16@gmail.com>
2023-02-20 12:17:38 +0530

    changes

--- JavaScript/API/Newsletter-Signup/app.js
+++ JavaScript/API/Newsletter-Signup/app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/",function(req,res){
-    console.log("Server is up");
-});
 
-app.listen("3000",function(){
+app.listen(3000,function(){
     console.log("Server is running in port 3000");
 });
