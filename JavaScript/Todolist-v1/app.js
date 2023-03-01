97f76256911df6e5ab5a5eccb0827936a9312861
Ullasachar16 <ullasachar16@gmail.com>
2023-03-01 13:25:46 +0530

    added res.render

--- JavaScript/Todolist-v1/app.js
+++ JavaScript/Todolist-v1/app.js
@@ -8,10 +8,13 @@ app.set('view engine','ejs');
 app.get("/",function(req,res){
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
     if(currentDay === 6|| currentDay === 0){
-        res.write("<h1>Yay its the weekend!</h1>");
+        day = "Weekend";
+        res.render("list",{kindOfDay: day});
     }else{
-        res.sendFile(__dirname + "/index.html");
+        day = "Weekday";
+        res.render("list",{kindOfDay: day});
     }
 });
 
