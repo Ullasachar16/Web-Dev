5b7355e0fbde1f18b1d9ce814906491513faf528
Ullasachar16 <ullasachar16@gmail.com>
2023-03-01 14:08:08 +0530

    changes

--- JavaScript/Todolist-v1/app.js
+++ JavaScript/Todolist-v1/app.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,11 +17,12 @@ app.get("/",function(req,res){
         month: "long"
     };
     var day = today.toLocaleDateString("en-US",options);
-    res.render("list",{kindOfDay: day,newListItem: item});
+    res.render("list",{kindOfDay: day,newListItems: item});
 });
 
 app.post("/",function(req,res){
-    var item = req.body.newItem;
+    item = req.body.newItem;
+    items.push(item);
     res.redirect("/");
 });
 
