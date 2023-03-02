cde63cf418a221672bce4ad0c271bccd2d666b80
Ullasachar16 <ullasachar16@gmail.com>
2023-03-02 13:47:43 +0530

    changes

--- JavaScript/Todolist-v1/app.js
+++ JavaScript/Todolist-v1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -12,14 +13,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US",options);
+
+    let day = date.getDate();
     res.render("list",{listTitle: day,newListItems: items});
+
 });
 
 app.post("/",function(req,res){
@@ -35,6 +32,10 @@ app.post("/",function(req,res){
 
 app.get("/work",function(req,res){
     res.render("list",{listTitle: "Work List",newListItems: workItems});
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
 });
 
 app.post("/work",function(req,res){
