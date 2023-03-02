b1cf05e400d96b64328a3e45a246855a8b9076de
Ullasachar16 <ullasachar16@gmail.com>
2023-03-02 12:46:42 +0530

    added post method for that

--- JavaScript/Todolist-v1/app.js
+++ JavaScript/Todolist-v1/app.js
@@ -32,6 +32,11 @@ app.get("/work",function(req,res){
     res.render("list",{listTitle: "Work List",newListItems: workItems});
 });
 
+app.post("/work",function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+});
+
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 });
