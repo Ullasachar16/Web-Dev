c533200b432a648f10638a139701bd93e55d00ad
Ullasachar16 <ullasachar16@gmail.com>
2023-03-05 12:22:38 +0530

    added post

--- JavaScript/Boss Level Challenge 3 - The Blog Website/app.js
+++ JavaScript/Boss Level Challenge 3 - The Blog Website/app.js
@@ -48,11 +48,12 @@ app.get("/posts/:postName",function(req,res){
     
     posts.forEach(function(post){
         const storedTitle = _.lowerCase(post.title);
-        
+
         if(storedTitle === requestedTitle){
-            console.log("Match Found!");
-        }else{
-            console.log("Not a Match");
+            res.render("post",{
+                title: post.title,
+                content: post.content
+            });
         }
     });
 });
