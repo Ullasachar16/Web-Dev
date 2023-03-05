5b7657944bafaae129b9af88a7f06504d262d568
Ullasachar16 <ullasachar16@gmail.com>
2023-03-05 11:21:31 +0530

    added post into home page

--- JavaScript/Boss Level Challenge 3 - The Blog Website/app.js
+++ JavaScript/Boss Level Challenge 3 - The Blog Website/app.js
@@ -13,6 +13,8 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var posts = [];
+
 app.get("/",function(req,res){
     res.render("home",{startingContent: homeStartingContent});
 });
@@ -30,10 +32,12 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-    var post = {
+    const post = {
         title: req.body.postTitle,
         content: req.body.postBody
     };
+    posts.push(post);
+    res.redirect("/");
 });
 
 app.listen(3000,function(){
