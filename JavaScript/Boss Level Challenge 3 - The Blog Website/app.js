ad96a3143dd1bd8c85f107b2510705cecdbd7dc2
Ullasachar16 <ullasachar16@gmail.com>
2023-03-05 11:16:35 +0530

    created object

--- JavaScript/Boss Level Challenge 3 - The Blog Website/app.js
+++ JavaScript/Boss Level Challenge 3 - The Blog Website/app.js
@@ -30,8 +30,10 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-    console.log(req.body.postTitle);
-    console.log(req.body.postBody);
+    var post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    };
 });
 
 app.listen(3000,function(){
