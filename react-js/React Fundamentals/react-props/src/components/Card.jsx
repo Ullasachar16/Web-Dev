65867745805f5d9cb9eaa1994bfcb69ac82c1db9
Ullasachar16 <ullasachar16@gmail.com>
2023-04-06 11:48:50 +0530

    added react props

---
+++ react-js/React Fundamentals/react-props/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Card(props){
+    return(
+        <div>
+            <div>
+                <h2>My Contacts</h2>
+                <img src="" alt="avatar_img"></img>
+            </div>
+            <div>
+                <p>phone</p>
+                <p>email</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
