624297f4b7f929a582d15295920e2a2c3084bdd5
Ullasachar16 <ullasachar16@gmail.com>
2023-04-06 12:48:16 +0530

    created another function and passed on to map

--- react-js/React Fundamentals/react-props/src/components/App.jsx
+++ react-js/React Fundamentals/react-props/src/components/App.jsx
@@ -4,6 +4,7 @@ import contacts from "../contacts";
 
 function createCard(contacts){
     return <Card 
+    id={contacts.id}
     key={contacts.id}
     name={contacts.name}
     img={contacts.imgURL}
