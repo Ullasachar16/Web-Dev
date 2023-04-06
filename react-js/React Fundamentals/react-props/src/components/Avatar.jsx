7b9fbfcba068460ece997aaf50348a4a0d4cc778
Ullasachar16 <ullasachar16@gmail.com>
2023-04-06 12:00:33 +0530

    added avatar image to card through props

--- react-js/React Fundamentals/react-props/src/components/Avatar.jsx
+++ react-js/React Fundamentals/react-props/src/components/Avatar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import App from "./App";
 
-function Avatar(){
+function Avatar(props){
     return <img src={props.img} alt="avatar_img" className="circle-img"></img>
 }
 
--- react-js/React Fundamentals/react-props/src/components/Card.jsx
+++ react-js/React Fundamentals/react-props/src/components/Card.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Avatar from "./Avatar";
 
 function Card(props){
     return(
         <div className="card">
             <div className="top">
                 <h2 className="name">{props.name}</h2>
-                <img src={props.img} alt="avatar_img" className="circle-img"></img>
-            </div>
+                <Avatar />
+                </div>
             <div className="bottom">
                 <p className="info">{props.tel}</p>
                 <p className="info">{props.email}</p>
