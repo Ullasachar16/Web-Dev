580494be24a776601e30b8c29b2d54d4dd8fb6dd
Ullasachar16 <ullasachar16@gmail.com>
2023-04-06 11:52:18 +0530

    added props to card.jsx

--- react-js/React Fundamentals/react-props/src/components/Card.jsx
+++ react-js/React Fundamentals/react-props/src/components/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Card(props){
     return(
-        <div>
-            <div>
-                <h2>My Contacts</h2>
-                <img src="" alt="avatar_img"></img>
+        <div className="card">
+            <div className="top">
+                <h2 className="name">{props.name}</h2>
+                <img src={props.img} alt="avatar_img"></img>
             </div>
-            <div>
-                <p>phone</p>
-                <p>email</p>
+            <div className="bottom">
+                <p className="info">{props.tel}</p>
+                <p className="info">{props.email}</p>
             </div>
         </div>
     );
