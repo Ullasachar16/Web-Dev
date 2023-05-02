a4cab72b709fdd07180b82882ded0967eab67840
Ullasachar16 <ullasachar16@gmail.com>
2023-05-02 11:20:22 +0530

    trying out

--- react-js/React Fundamentals/react-object-and-array-destructing/src/index.js
+++ react-js/React Fundamentals/react-object-and-array-destructing/src/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import animals from './data';
 
-ReactDOM.render(
-      <table>
-        <tr>
-          <th>Brand</th>
-          <th>Top Speed</th>
-        </tr>
-        <tr>
-          <td>{tesla.model}</td>
-          <td>{teslaTopSpeed}</td>
-          <td>{teslaTopColour}</td>
-        </tr>
-        <tr>
-          <td>{honda.model}</td>
-          <td>{hondaTopSpeed}</td>
-          <td>{hondaTopColour}</td>
-        </tr>
-      </table>,
-      document.getElementById("root")
-    );
+console.log(animals);
+
+const [cat,dog] = animals;
+
+console.log(cat);
+
+const {name,sound} = cat;
+
+console.log(sound);
+
+// ReactDOM.render(
+//       <table>
+//         <tr>
+//           <th>Brand</th>
+//           <th>Top Speed</th>
+//         </tr>
+//         <tr>
+//           <td>{tesla.model}</td>
+//           <td>{teslaTopSpeed}</td>
+//           <td>{teslaTopColour}</td>
+//         </tr>
+//         <tr>
+//           <td>{honda.model}</td>
+//           <td>{hondaTopSpeed}</td>
+//           <td>{hondaTopColour}</td>
+//         </tr>
+//       </table>,
+//       document.getElementById("root")
+//     );
     
