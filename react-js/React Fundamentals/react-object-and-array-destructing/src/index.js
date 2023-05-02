c309d66e11f57ee472dd12b8c5c3471aba13bbd7
Ullasachar16 <ullasachar16@gmail.com>
2023-05-02 11:13:43 +0530

    changing challenge code

--- react-js/React Fundamentals/react-object-and-array-destructing/src/index.js
+++ react-js/React Fundamentals/react-object-and-array-destructing/src/index.js
@@ -1,4 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />,document.getElementById("root"));
+ReactDOM.render(
+      <table>
+        <tr>
+          <th>Brand</th>
+          <th>Top Speed</th>
+        </tr>
+        <tr>
+          <td>{tesla.model}</td>
+          <td>{teslaTopSpeed}</td>
+          <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+          <td>{honda.model}</td>
+          <td>{hondaTopSpeed}</td>
+          <td>{hondaTopColour}</td>
+        </tr>
+      </table>,
+      document.getElementById("root")
+    );
+    
