268588e0a62d11f4e83f01d32949363eed501b53
Ullasachar16 <ullasachar16@gmail.com>
2023-05-02 11:32:21 +0530

    challenge code added

--- react-js/React Fundamentals/react-object-and-array-destructing/src/index.js
+++ react-js/React Fundamentals/react-object-and-array-destructing/src/index.js
@@ -2,29 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import animals from './data';
 
-const [cat,dog] = animals;
-
-const {feedingRequirements: {food,water}}= cat;
-
-console.log(food);
-
-// ReactDOM.render(
-//       <table>
-//         <tr>
-//           <th>Brand</th>
-//           <th>Top Speed</th>
-//         </tr>
-//         <tr>
-//           <td>{tesla.model}</td>
-//           <td>{teslaTopSpeed}</td>
-//           <td>{teslaTopColour}</td>
-//         </tr>
-//         <tr>
-//           <td>{honda.model}</td>
-//           <td>{hondaTopSpeed}</td>
-//           <td>{hondaTopColour}</td>
-//         </tr>
-//       </table>,
-//       document.getElementById("root")
-//     );
+ReactDOM.render(
+      <table>
+        <tr>
+          <th>Brand</th>
+          <th>Top Speed</th>
+        </tr>
+        <tr>
+          <td>{tesla.model}</td>
+          <td>{teslaTopSpeed}</td>
+          <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+          <td>{honda.model}</td>
+          <td>{hondaTopSpeed}</td>
+          <td>{hondaTopColour}</td>
+        </tr>
+      </table>,
+      document.getElementById("root")
+    );
     
