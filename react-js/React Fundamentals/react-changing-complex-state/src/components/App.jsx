a2ca64f7e018ae74e5a0636873a421925771541a
Ullasachar16 <ullasachar16@gmail.com>
2023-05-05 12:08:32 +0530

    added email also

--- react-js/React Fundamentals/react-changing-complex-state/src/components/App.jsx
+++ react-js/React Fundamentals/react-changing-complex-state/src/components/App.jsx
@@ -1,53 +1,68 @@
 import React, { useState } from "react";
 
-function App(){
-
-    const [fullName,setFullName] = useState({
-        fName: "",
-        lName: ""
+function App() {
+    const [contact, setContact] = useState({
+      fName: "",
+      lName: "",
+      email: ""
     });
-
-    function handleChange(event){
-
-        const {value,name} = event.target;
-
-        setFullName((previousValue) => {
-
-            if(name === "fName"){
-                return {
-                    fName: value,
-                    lName: previousValue.lName
-                }
-            }else if(name === "lName"){
-                return {
-                    fName: previousValue.fName,
-                    lName: value
-                };
-            }
-        });
+  
+    function handleChange(event) {
+      const { name, value } = event.target;
+  
+      setContact(prevValue => {
+        if (name === "fName") {
+          return {
+            fName: value,
+            lName: prevValue.lName,
+            email: prevValue.email
+          };
+        } else if (name === "lName") {
+          return {
+            fName: prevValue.fName,
+            lName: value,
+            email: prevValue.email
+          };
+        } else if (name === "email") {
+          return {
+            fName: prevValue.fName,
+            lName: prevValue.lName,
+            email: value
+          };
+        }
+      });
     }
-
   
     return (
-
-        <div className="container">
-        <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <div className="container">
+        <h1>
+          Hello {contact.fName} {contact.lName}
+        </h1>
+        <p>{contact.email}</p>
         <form>
-        <input 
-        onChange={handleChange}
-        name = "fName" placeholder="First Name" 
-        value={fullName.fName}
-        />
-        <input 
-        onChange={handleChange}
-        name = "lName" placeholder="Last Name"
-        value={fullName.lName} 
-        />
-        <button>Submit</button>
+          <input
+            onChange={handleChange}
+            value={contact.fName}
+            name="fName"
+            placeholder="First Name"
+          />
+          <input
+            onChange={handleChange}
+            value={contact.lName}
+            name="lName"
+            placeholder="Last Name"
+          />
+          <input
+            onChange={handleChange}
+            value={contact.email}
+            name="email"
+            placeholder="Email"
+          />
+          <button>Submit</button>
         </form>
-        </div>
+      </div>
     );
-
-}
+  }
+  
 
 export default App;
